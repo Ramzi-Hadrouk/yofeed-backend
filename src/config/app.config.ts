116632e879b85af58778310830e353fbcd054e66
Ramzi Hadrouk <hadrouk_ramzi_@hotmail.com>
2025-08-28 14:00:50 +0100

    feat: enhance app config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,6 @@ import { registerAs } from '@nestjs/config';
 export const appConfig = registerAs('app', () => ({
   nodeEnv: process.env.NODE_ENV,
   port: Number(process.env.PORT),
-  jwtSecret: process.env.JWT_SECRET,
   apiRateLimit: Number(process.env.API_RATE_LIMIT),
   logLevel: process.env.LOG_LEVEL,
   logFileEnabled: process.env.LOG_FILE_ENABLED === 'true',
